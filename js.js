2a13791dfbf00b7648c0694f3d34520ab5ef7d3d
Kristina Kirnos <kristina_918@mail.ru>
2025-08-27 18:49:35 +0300

    Initial commit

---
+++ js.js
@@ -0,0 +1,41 @@
+//Задание 1
+let myInfo = {
+  name: "Кристина",
+  age: 30,
+  homePets: true,
+  favoriteBook,
+};
+//Задание 2
+
+"JavaScript" - //string
+  2024 - // number
+  true - // boolean логические значения
+  null - //  отсутствует какое либо значение
+  undefined; //- значение не было присвоено
+
+//Задание №3
+
+let user = { name: "Alice", age: 30 };
+console.log(typeof user); //object
+
+function sayHello() {
+  console.log("Hello!");
+}
+console.log(typeof sayHello); //function
+
+let combined = "Age: " + 25;
+console.log(typeof combined); //string
+
+//Задание 4
+
+let person = {
+  firstName: "Kristina",
+  lastName: "Kirnos",
+  age: 30,
+  isEmployed: true,
+  contact: {
+    email: "kristina_918@mail.ru",
+    phone: undefined,
+    uniqueId: Symbol("id"),
+  },
+};
