2e0a1a30da26b59ac75bb7c1bfa55700e3f56006
Kristina <kristina_918@mail.ru>
2025-09-14 20:00:37 +0600

    ДЗ 5 урока

--- dist/js.dev.js
+++ dist/js.dev.js
@@ -1,3 +1,13 @@
+"use strict";
+
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
 /*Задание №1 — Сложение числа и строки
 
 Напишите функцию combineValue, которая принимает два параметра: number (число) и text (строка).
@@ -6,7 +16,6 @@
 Пример к заданию №1: combineValue(5, "яблок") // "5 яблок" или combineValue(-3, "яблок") // "Некорректное значение"
 
 */
-
 function combineValue(number, text) {
   if (number < 0) {
     alert("Некорректное значение");
@@ -16,9 +25,7 @@ function combineValue(number, text) {
 }
 
 combineValue(3, "Антон");
-
 combineValue(-3, "Антон");
-
 /*
 Задание №2 — Умножение числа на два
 
@@ -32,7 +39,6 @@ function doubleNumber(number) {
 }
 
 console.log(doubleNumber(prompt("Введите число")));
-
 /*
 Задание №3 — Преобразование строки в число
 
@@ -49,8 +55,8 @@ console.log(doubleNumber(prompt("Введите число")));
 function convertToNumber(text) {
   return Number(text);
 }
-console.log(convertToNumber(prompt("Введите число")));
 
+console.log(convertToNumber(prompt("Введите число")));
 /*
 
 Задание №4 — Функция с массивом
@@ -61,19 +67,20 @@ console.log(convertToNumber(prompt("Введите число")));
 Пример к заданию №4: findMaxNumber([1, 5, 9, 2]) → 9 или findMaxNumber([]) → "Массив пустой"*/
 
 function createArray() {
-  let arr = [];
-
+  var arr = [];
   arr.push(prompt("Введите первое число:"));
   arr.push(prompt("Введите второе число:"));
   arr.push(prompt("Введите третье число:"));
-
-  let numbers = arr.map((item) => Number(item));
-
+  var numbers = arr.map(function (item) {
+    return Number(item);
+  });
   return numbers;
 }
 
 function findMaxNumber(numbers) {
   numbers = createArray();
-  return Math.max(...numbers);
+  return Math.max.apply(Math, _toConsumableArray(numbers));
 }
-alert("наибольшее число: " + (findMaxNumber()));
+
+alert("наибольшее число: " + findMaxNumber());
+//# sourceMappingURL=js.dev.js.map
