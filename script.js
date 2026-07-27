c06beaa12987900cbfcbaaa852b88bcfc527ea7e
Kristina <kristina_918@mail.ru>
2026-07-27 11:58:38 +0600

    lesson13

--- script.js
+++ script.js
@@ -1,73 +1,101 @@
-// Вы планируете выйти погулять днем. Если на улице дождь, то надо взять зонтик. Если на улице солнце, то надо взять солнечные очки.
+let formSignUp = document.getElementById("signupForm");
 
-// if
+// Событие submit
 
-let isFridgeEmpty = true;
+formSignUp.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-if (isFridgeEmpty === true) {
-  console.log("Холодильник пуст, надо сходить в магазин");
-}
+  // Список полей
+  let usernameInput = document.getElementById("username");
+  let username = document.getElementById("username").value;
+  let emailInput = document.getElementById("email");
+  let email = document.getElementById("email").value;
+  let passwordInput = document.getElementById("password");
+  let password = document.getElementById("password").value;
+  let confirmPasswordInput = document.getElementById("confirmPassword");
+  let confirmPassword = document.getElementById("confirmPassword").value;
 
-// if...else
+  // Блок для ошибок
 
-let age = 15;
+  let usernameError = document.getElementById("usernameError");
+  let emailError = document.getElementById("emailError");
+  let passwordError = document.getElementById("passwordError");
+  let confirmPasswordError = document.getElementById("confirmPasswordError");
 
-if (age >= 18) {
-  console.log("Доступ разрешён");
-} else {
-  console.log("Доступ запрещён");
-}
+  // Сброс текста в ошибке
+  usernameError.textContent = "";
+  emailError.textContent = "";
+  passwordError.textContent = "";
+  confirmPasswordError.textContent = "";
 
-// тернарный оператор для if...else
-let grade = 4;
+  let isValid = true;
 
-console.log(
-  grade === 5
-    ? "Отлично"
-    : grade === 4
-      ? "Хорошо"
-      : grade === 3
-        ? "Удовлетворительно"
-        : "Требуется пересдача",
-);
+  // Условия для поля username
 
-let hour = 14;
+  if (!username) {
+    usernameError.textContent = "Введите имя пользователя";
+    usernameInput.style.borderColor = "red";
+    isValid = false;
+  } else {
+    usernameInput.style.borderColor = "green";
+  }
+  if (username.length < 4) {
+    usernameError.textContent =
+      "Имя пользователя должно быть не менее 4 символов";
+    usernameInput.style.borderColor = "red";
+    isValid = false;
+  } else {
+    usernameInput.style.borderColor = "green";
+  }
 
-console.log(hour < 12 ? "Доброе утро!" : "Добрый день!");
+  // Условия для поля email
 
-// if...else if...else
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-let weather = "cloudy";
+  if (!email) {
+    emailError.textContent = "Введите email";
+    isValid = false;
+    emailInput.style.borderColor = "red";
+  } else if (!emailPattern.test(email)) {
+    emailError.textContent = "Некорректный email";
+    isValid = false;
+    emailInput.style.borderColor = "red";
+  } else {
+    emailInput.style.borderColor = "green";
+  }
 
-console.log(
-  weather === "rain"
-    ? "Возьмите зонтик"
-    : weather === "sunny"
-      ? "Возьмите солнечные очки"
-      : "Непонятно, что брать!",
-);
+  // Условия для поля password
 
+  if (!password) {
+    passwordError.textContent = "Введите пароль";
+    isValid = false;
+    passwordInput.style.borderColor = "red";
+  } else if (password.length < 6) {
+    passwordError.textContent = "Пароль должен быть не менее 6 символов";
+    isValid = false;
+    passwordInput.style.borderColor = "red";
+  } else {
+    passwordInput.style.borderColor = "green";
+  }
 
+  // Условия для поля confirm password
 
-// оператор switch
-/*
-let weather = "snow";
+  if (password !== confirmPassword) {
+    confirmPasswordError.textContent = "Пароли не совпадают";
+    isValid = false;
+    confirmPasswordInput.style.borderColor = "red";
+  }
+  if (confirmPassword.length < 1) {
+    confirmPasswordError.textContent = "Установите пароль";
+    isValid = false;
+    confirmPasswordInput.style.borderColor = "red";
+  } else {
+    confirmPasswordInput.style.borderColor = "green";
+  }
 
-switch (weather) {
-  case "rain":
-    console.log("Возьмите зонтик!");
-    break;
-  case "sunny":
-    console.log("Возьмите солнечные очки!");
-    break;
-  case "snow":
-    console.log("Наденьте пуховик и шапку!");
-    break;
-  case "cloudy":
-    console.log("Наденьте куртку!");
-    break;
-  default:
-    console.log("Непонятная погода, ничего не берите!");
-    break;
-}
-*/
+  // Проверка формы на true по всем условиям выше
+
+  if (isValid) {
+    alert("Форма отправлена успешно!");
+  }
+});
